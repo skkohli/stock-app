f2bc68a5a7df4e29c2d886ac16f1b3cf0f6ac0ce
skkohli <kohli.iitm@gmail.com>
2014-12-07 14:06:05 +0530

    commented the code for stock.js directive

--- app/scripts/directives/stock.js
+++ app/scripts/directives/stock.js
@@ -7,6 +7,7 @@ app.directive('stockApp',['$http','$timeout',function($http,$timeout){
 			scope.canSell = false;
 			scope.stocksData = [];
 			scope.i = 0;
+			// initialize the portfolio data in local storage. if data is already stored then update the stocks data
 			if(localStorage.getItem('portfolio') === null){
 				scope.portfolio = {funds:100000,stocks:{}};   		  		
 	       		  	//console.log('initial data null');
@@ -15,19 +16,23 @@ app.directive('stockApp',['$http','$timeout',function($http,$timeout){
 		  	scope.updateStocksData(); 
 		  	//scope.stocksData = scope.portfolio.stocks;
 	    }
-
 	    localStorage.setItem('portfolio',JSON.stringify(scope.portfolio));
+	    /*error handler for throwing error. we have set timeout for errors we want to show only for a small time*/
 	    scope.throwError = function(msg,setTimeout){
     		scope.error = true;
 				scope.errorMsg = msg;
+				//set timeout to show error for 5 secs
 				if(setTimeout)
 					$timeout(function(){scope.error = false;scope.errorMsg = null}, 5000);       
 	    }
 
 		},
+		/*
+		controller for business logic of directive
+		*/
 		controller:function($scope){
 			var url = "../api/get_data.php";
-
+			/*code to talk to server and get data */
 			$scope.searchSymbol = function(symbol) {
 				var query = symbol;
 				if(!symbol){
@@ -39,6 +44,7 @@ app.directive('stockApp',['$http','$timeout',function($http,$timeout){
 					return;
 				}
 				$scope.showdata = false;
+				//communicate with the server and put data in data variable of scope
 				$http({
 					method: 'GET',
 					url: url+'?q='+query
@@ -54,8 +60,10 @@ app.directive('stockApp',['$http','$timeout',function($http,$timeout){
 					}
 	      });
 	     };
+	     //business logic of buy function
 			$scope.buy = function(){
 				var stockQuantity = parseInt($("#stock-quantity").val());
+				//validating input to be a number
 				if(isNaN(stockQuantity)){
 					$scope.throwError('No of shares should be a number',true);
 					return;
@@ -72,18 +80,21 @@ app.directive('stockApp',['$http','$timeout',function($http,$timeout){
 					$scope.throwError('insufficient funds',true);
 				}
 			},
+			/*logic to show the sell button only if shares are bought for the company*/
 			$scope.setCanSell = function() {
 				$scope.canSell =true;
 	  	  // console.log($scope.portfolio);
 	  	  if($scope.portfolio.stocks[$scope.data.symbol] === undefined || $scope.portfolio.stocks[$scope.data.symbol].quantity == 0)
 	  	  	$scope.canSell = false;
 	    },
+	    /*validate symbol typed in search box*/
 	    $scope.validateSymbol = function(symbol){
 	    	var letters = /^[0-9a-zA-Z]+$/;
 	    	if(symbol.match(letters))
 	    		return true;
 	    	return false;
 	    }
+	    /*logic for selling shares*/
 	  	$scope.sell = function() {
 	  		var stockQuantity = parseInt($("#stock-quantity").val());
 	  		if(isNaN(stockQuantity)) {
@@ -104,6 +115,7 @@ app.directive('stockApp',['$http','$timeout',function($http,$timeout){
 	   	  	$scope.throwError("insufficient shares",true);
 	   	  }
 	    },
+	    /*update stocks data array for ng-repeat binding*/
 	   	$scope.updateStocksData = function(){
 	   		$scope.portfolio =JSON.parse(localStorage.getItem('portfolio')); 
 	   		$scope.stocksData = [];
@@ -114,18 +126,20 @@ app.directive('stockApp',['$http','$timeout',function($http,$timeout){
 	   			$scope.i++;
 	   		}
 	   	},
+	   	/*validate if user can buy shares based on amount of funds*/
 	   	$scope.validateBuy = function(quantity){
 		  	//var portfolio = JSON.parse(localStorage.getItem('portfolio'));
 		  	if(quantity*($scope.data.ask)<=$scope.portfolio.funds)
 		  		return true;
 		  	return false;
 	  	},
+	  	/*validate if user has enough shares of company to sell*/
 	    $scope.validateSell = function(quantity){
 	    	if(quantity<=$scope.portfolio.stocks[$scope.data.symbol].quantity)
 	    		return true;
 	    	return false;
 	    },
-
+	    /*update portfolio after buy or sell*/
 		  $scope.updatePortfolio = function(stockQuantity){
 		  	var stocks = $scope.portfolio['stocks'];
 		  	var symbol = $scope.data.symbol;
@@ -148,6 +162,7 @@ app.directive('stockApp',['$http','$timeout',function($http,$timeout){
 		    $scope.portfolio.stocks = stocks;
 		  }
 		},
+		//template url for external template
 	  templateUrl:'views/dashboard.html',
 
 	};

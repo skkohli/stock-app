290a29c32e415cf3110d751f769991cd56a44252
skkohli <kohli.iitm@gmail.com>
2014-12-07 11:49:29 +0530

    added comments

--- api/get_data.php
+++ api/get_data.php
@@ -1,5 +1,6 @@
 <?php
 require 'Curl.php';
+//Not filtering $_GET as data is filtered at front end and at the API layer
 $url = 'http://data.benzinga.com/stock/'.$_GET['q'];
 use \Curl\Curl;
 $cur = new Curl();
